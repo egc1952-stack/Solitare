f1b9df0c8317739c53cfed34d4a2042680e8c9e3
Eric Cutler <ericcutler@comcast.net>
2026-05-25 11:57:53 -0700

    Refactor VSCode settings and tasks, enhance initialization logging, and update binary files

--- initialize.cpp
+++ initialize.cpp
@@ -9,7 +9,7 @@ int initialize() {
     sqlite3* db = nullptr;
 
     // Initialize game state, load resources, etc.
-    std::cout << "Initializing Solitaire..." << std::endl;
+				std::cout << "Initializing Solitaire..." << std::endl;
     // open SQLite database
     db_open("Solitaire.db");
     std::cout << "SQLite opened successfully.\n";
@@ -18,7 +18,15 @@ int initialize() {
     // if deals exist, then start worker threads.
     const char*sql="select count(*) from Deal_Head where EndTime=0";
     std::cout << sql << "\n";
-    db_query(sql);
+    sqlite3_stmt* stmt = db_query(sql);
+    if (sqlite3_step(stmt) == SQLITE_DONE) 
+        {
+            // create new deal
+            std::cout << "New Deal Needed.\n";
+
+        }
+
+   
 
 
     return 0;
